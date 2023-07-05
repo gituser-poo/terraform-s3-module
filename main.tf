211722092fd485c4e52decd22c578b73ec3149a0
gituser-poo <137917469+gituser-poo@users.noreply.github.com>
2023-07-05 13:59:09 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+    region = "ap-south-1"
+    access_key = "AKIA3D36VHA2AUBPFME3"
+    secret_key = "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"
+}
+
+module "s3_bucket" {
+    source = ".//module-1"
+}
